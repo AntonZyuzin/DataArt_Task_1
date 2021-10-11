b3da13d74f9c5d14adb874f76b5cc18ad99535b0
AntonZyuzin <71172648+AntonZyuzin@users.noreply.github.com>
2021-10-11 14:03:18 +0300

    Update LongestSubstring.java

--- task/src/main/java/ru/dataart/academy/java/LongestSubstring.java
+++ task/src/main/java/ru/dataart/academy/java/LongestSubstring.java
@@ -9,13 +9,28 @@ public class LongestSubstring {
      * Example: dnmdncbb  -> 5 (mdncb)
      * amam -> 2 (am)
      */
-
     public int getLengthOfLongestSubstring(String checkString) {
         char[] str = checkString.toCharArray();
         ArrayList<Character> _listOfLetters = new ArrayList<>();
-        for (char item : str) {
-            if (!_listOfLetters.contains(item)) _listOfLetters.add(item);
+        ArrayList<Integer> _listOfSizes = new ArrayList<>();
+
+        for (char i : str) {
+            if (_listOfLetters.contains(i)) {
+                _listOfSizes.add(_listOfLetters.size());
+                _listOfLetters.clear();
+            }
+            _listOfLetters.add(i);
         }
-        return _listOfLetters.size();
+        _listOfSizes.add(_listOfLetters.size());
+
+        for (int j = str.length - 1; j > 0; j--) {
+            if (_listOfLetters.contains(str[j])) {
+                _listOfSizes.add(_listOfLetters.size());
+                _listOfLetters.clear();
+            }
+            _listOfLetters.add(str[j]);
+        }
+        _listOfSizes.add(_listOfLetters.size());
+        return Collections.max(_listOfSizes);
     }
 }
